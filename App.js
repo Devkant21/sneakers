67282429be2f8904b315e8dc9884795bd8e367bf
Devkant <devkant21@gmail.com>
2023-05-08 20:03:05 +0530

    ProductScreen

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, FlatList } from "react-native";
-import product from "./src/data/product";
+import { StyleSheet, View } from "react-native";
+import ProductScreen from "./src/screens/productScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={product}
-        renderItem={({ item } ) => (
-          <View style={styles.itemContainer}>
-            <Image source={{ uri: item.image }} style={styles.image} />
-          </View>
-        )}
-        numColumns={2}
-      />
+      <ProductScreen />
 
       <StatusBar style="auto" />
     </View>
@@ -26,13 +18,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  itemContainer: {
-    width: "50%",
-    padding: 1,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
   },
 });
---
+++ src/screens/ProductScreen.js
@@ -0,0 +1,31 @@
+import { StyleSheet, View, Image, FlatList } from "react-native";
+import product from "../data/product";
+
+const ProductScreen = () => {
+    return (
+        <FlatList
+            data={product}
+            renderItems={({ item }) => (
+                <View style={styles.itemContainer}>
+                    <Image source={{ uri: item.image }} style={styles.image} />
+                </View>
+            )}
+            numColumns={2}
+            
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    itemContainer: {
+        width: '50%',
+        padding: 1,
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+    },
+});
+
+
+export default ProductScreen;
